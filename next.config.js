a9a3009994148832c2263079b34aa9721a204708
Hammad <hammad4299@gmail>
2023-10-21 00:23:15 +0500

    fix : build error

--- next.config.js
+++ next.config.js
@@ -19,33 +19,12 @@ const nextConfig = {
       // options.nextRuntime
     );
     if (options.nextRuntime === "nodejs") {
-      const oldEntry = config.entry;
-      config.entry = () =>
-        new Promise(async (resolve) => {
-          resolve({
-            ...(await oldEntry()),
-            migrator: "src/app/Database/migrations/DatabaseClient/index.ts",
-          });
-        });
       config.externals.push("sqlite3");
       config.externals.push("sequelize");
-      config.plugins.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: path.resolve(
-                __dirname,
-                "./src/app/Database/migrations/migrations"
-              ),
-              to: path.resolve(__dirname, ".next/server/migrations"),
-            },
-          ],
-        })
-      );
     }
     console.log(
       "entry after",
-      // await config.entry(),
+
       options.isServer,
       options.nextRuntime
     );
